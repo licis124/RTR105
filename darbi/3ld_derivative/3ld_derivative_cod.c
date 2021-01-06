acc6b0976a09058d84f36336501d658f1cd7a5a2
Eriks Licis <71380662+licis124@users.noreply.github.com>
2021-01-06 15:51:44 +0200

    Update 3ld_derivative_cod.c

--- darbi/3ld_derivative/3ld_derivative_cod.c
+++ darbi/3ld_derivative/3ld_derivative_cod.c
@@ -15,7 +15,7 @@ printf("x\t\tj0(x)\t\tj0\’(x)\t\t∆j0(x)\t\-j1\’(x)\t\t∆-j0(x)\n");
   x = a;
   while(x<=b)
   {
-      printf("%10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\n",x,j0(x),-j1(x),(j0(x+delta_x)-j0(x))/delta_x,-j0(x),(-j0(x+delta_x)-j0(x))/delta_x);
+      printf("%10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\n",x,j0(x),-j1(x),(j0(x+delta_x)-j0(x))/delta_x,-j0(x),(-j0(x+delta_x)-j0(x))/delta_x);
       x += delta_x;
   }
 }
