09ba8016c5b324603668cd7aeac24eeeb121b224
Eriks Licis <71380662+licis124@users.noreply.github.com>
2021-01-08 10:28:35 +0200

    Update 5ld_statistics.c

--- darbi/5ld_statisctic/5ld_statistics.c
+++ darbi/5ld_statisctic/5ld_statistics.c
@@ -28,12 +28,31 @@ void printArray(int arr[], int size)
         printf("%d ", arr[i]); 
     printf("\n"); 
 } 
+int mode(int arr[],int n) {
+   int maxValue = 0, maxCount = 0, i, j;
+
+   for (i = 0; i < n; ++i) {
+      int count = 0;
+      
+      for (j = 0; j < n; ++j) {
+         if (arr[j] == arr[i])
+         ++count;
+      }
+      
+      if (count > maxCount) {
+         maxCount = count;
+         maxValue = arr[i];
+      }
+   }
+
+   return maxValue;
+}
   
 // Driver program to test above functions 
 int main() 
-{  int i, max, min, N,average;
+{  int i, max, min, N;
 int * arr;
-float sum, mediana ;
+float sum, mediana,average ;
     printf("Enter size of the array: ");
     scanf("%d", &N);
     arr=(int*)malloc (i);
@@ -45,13 +64,7 @@ float sum, mediana ;
         scanf("%d", &arr[i]);
          sum += arr[i];
     }
-    if(N%2)
-        mediana= arr[N/2];
-    else
-        mediana= (arr[N/2]+arr[N/2-1])/2;
-    average = sum/N;
-    max = arr[0];
-    min = arr[0];
+    
 
     for(i=1; i<N; i++)
     {
@@ -70,7 +83,17 @@ int n = sizeof(arr)/sizeof(arr[0]);
     bubbleSort(arr, N); 
     printf("Sorted array: \n"); 
     printArray(arr, N); 
+    if(N%2)
+        mediana= arr[N/2];
+    else
+        mediana= (arr[N/2]+arr[N/2-1])/2;
+    average = sum/N;
+    max = arr[0];
+    min = arr[0];
      printf("Maximum element = %d\n", max);
     printf("Minimum element = %d\n", min);
-    printf("Average = %d\n", average);
+    printf("Average = %.2f\n", average);
+    printf("Mediana = %.3f\n", mediana);
+    printf("Mode = %d ", mode(arr,N));
     return 0; 
+}
