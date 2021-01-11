ad7bd279f7df06db83b885b2fdd1c2010896c9df
Eriks Licis <71380662+licis124@users.noreply.github.com>
2021-01-11 13:49:58 +0200

    Update 5ld_statistics.c

--- darbi/5ld_statisctic/5ld_statistics.c
+++ darbi/5ld_statisctic/5ld_statistics.c
@@ -33,12 +33,12 @@ int mode(int arr[],int n) {
 
    for (i = 0; i < n; ++i) {
       int count = 0;
-
+      
       for (j = 0; j < n; ++j) {
          if (arr[j] == arr[i])
          ++count;
       }
-
+      
       if (count > maxCount) {
          maxCount = count;
          maxValue = arr[i];
@@ -47,33 +47,39 @@ int mode(int arr[],int n) {
 
    return maxValue;
 }
-
+  
 // Driver program to test above functions 
 int main() 
-{  int i, max, min, N,average;
 {  int i, max, min, N;
 int * arr;
-float sum, mediana ;
 float sum, mediana,average ;
     printf("Enter size of the array: ");
     scanf("%d", &N);
     arr=(int*)malloc (i);
- float sum, mediana ;
+    if(arr==NULL) exit(1);
+    printf("Size or array :%lu\n",sizeof(arr)/sizeof(arr[0]) );
+    printf("Enter elements in the array: \n");
+    for(i=0; i<N; i++)
+    {
         scanf("%d", &arr[i]);
          sum += arr[i];
     }
-    if(N%2)
-        mediana= arr[N/2];
-    else
-        mediana= (arr[N/2]+arr[N/2-1])/2;
-    average = sum/N;
-    max = arr[0];
-    min = arr[0];
-
+    
 
     for(i=1; i<N; i++)
     {
-int n = sizeof(arr)/sizeof(arr[0]);
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+int n = sizeof(arr)/sizeof(arr[0]); 
+     printf("Before sorted array: \n"); 
+    printArray(arr, N);
     bubbleSort(arr, N); 
     printf("Sorted array: \n"); 
     printArray(arr, N); 
@@ -86,7 +92,6 @@ int n = sizeof(arr)/sizeof(arr[0]);
     min = arr[0];
      printf("Maximum element = %d\n", max);
     printf("Minimum element = %d\n", min);
-    printf("Average = %d\n", average);
     printf("Average = %.2f\n", average);
     printf("Mediana = %.3f\n", mediana);
     printf("Mode = %d ", mode(arr,N));
